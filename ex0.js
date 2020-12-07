640e260ec9cef3c27f6f22083487d870a2727c52
oskytar <oscar.miras@tutor.bit.es>
2020-12-07 13:44:15 +0100

    errata

--- ex0.js
+++ ex0.js
@@ -3,7 +3,7 @@
  * 
  * Queremos escribir un script que, dado un mes del año, nos diga en que trimestre nos encomtramos.
  * 
- * Por ejemplo, si mes = 4, deberíamos mostrar por el terminal "Nos encontramos en el segundo cuatrimestre"  
+ * Por ejemplo, si mes = 4, deberíamos mostrar por el terminal "Nos encontramos en el segundo trimestre"  
  * 
  * Explra esta información para averiguar como usar el condicional 'switch'; y cómo se pueden agrupar varios casos: https://javascript.info/switch#grouping-of-case
  * 
